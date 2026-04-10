921bb983528dec731102d1b58a96acca14821604
David Prüser <david@pruesernet.de>
2026-04-10 11:47:22 +0200

    [RE] fixed relib dependency paths and building process

--- random_events/setup.py
+++ random_events/setup.py
@@ -1,19 +1,19 @@
 import ast
 
-from pybind11.setup_helpers import Pybind11Extension
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
 ext_modules = [
     Pybind11Extension(
-        "random_events_lib",  # Python module name
+        "random_events_lib",
         [
-            "src/random-events-lib/export/bindings.cpp",
-            "src/random-events-lib/random_events_lib/src/sigma_algebra.cpp",
-            "src/random-events-lib/random_events_lib/src/set.cpp",
-            "src/random-events-lib/random_events_lib/src/product_algebra.cpp",
+            "src/random_events_lib/export/bindings.cpp",
+            "src/random_events_lib/random_events_lib/src/sigma_algebra.cpp",
+            "src/random_events_lib/random_events_lib/src/set.cpp",
+            "src/random_events_lib/random_events_lib/src/product_algebra.cpp",
         ],  # C++ binding source
         include_dirs=[
-            "src/random-events-lib/random_events_lib/include"
+            "src/random_events_lib/random_events_lib/include"
         ],  # Include directory for C++ headers
         extra_compile_args=["-std=c++17", "-fPIC"],
     ),
@@ -45,4 +45,5 @@ setup(
     python_requires=">=3.8",
     install_requires=open("requirements.txt").read().splitlines(),
     ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
 )
--- random_events/src/random_events_lib/__init__.py
+++
@@ -1,2 +0,0 @@
-__title__ = 'random_events_lib'
-__version__ = '0.0.1'
